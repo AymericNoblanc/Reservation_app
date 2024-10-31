9e0670abbd22f9b20b44c69f832696d61e03796a
AymericNoblanc <aymeric.noblanc@gmail.com>
2024-10-31 23:41:15 +0100

    Meilleur gestion des sites et meilleur gestion de l'affichage des réservation notament au moment où on les supprimes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Fonction pour se connecter à la base de données PostgreSQL
+
 def get_db_connection():
     conn = psycopg2.connect(
         host=data["host"],
